ffd57e020875c67a7d15c1f7329e2b50facaa017
aqilusman45 <aqilusman45@gmail.com>
2025-03-11 10:27:45 +0500

    feat: add title & description and remove fonts

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,7 @@
-const Home = () => {
+export default function Home() {
   return (
     <>
-      <h1>Backstage</h1>
+      <h1 className="text-3xl font-bold underline">Hello Backstage!</h1>
     </>
   );
-};
-
-export default Home;
+}
