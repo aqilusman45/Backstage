c7393b53af4a63d845fcf654ef868738e7f87a76
aqilusman45 <aqilusman45@gmail.com>
2025-03-13 14:22:28 +0500

    fix: cta ui

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -13,18 +13,18 @@ const Button = ({
   const buttonClasses = classNames(
     "cursor-pointer font-instrument-sans transition-all duration-300 ease-in-out group relative overflow-hidden",
     {
-      "text-white bg-black px-medium-x py-medium-y  text-xxii rounded-full hover:bg-white transform hover:scale-[1.02] hover:shadow-md hover:text-black border border-2-black":
+      "text-white bg-black rounded-full hover:bg-white transform hover:scale-[1.02] hover:shadow-md hover:text-black border border-2-black":
         variant === "primary",
 
-      "text-black bg-white text-xxii rounded-full hover:bg-black hover:text-white transform hover:scale-[1.02]":
+      "text-black bg-white border border-white hover:border-black rounded-full hover:bg-black hover:text-white transform hover:scale-[1.02]":
         variant === "secondary",
 
-      "text-black border border-2-black text-black text-xxii rounded-full hover:bg-black hover:text-white transform hover:scale-[1.02]":
+      "text-black border border-2-black text-black rounded-full hover:bg-black hover:text-white transform hover:scale-[1.02]":
         variant === "outline",
 
-      "rounded-full font-medium text-xxii px-medium-x py-medium-y ":
+      "rounded-full font-medium text-lg xl:text-xxii px-medium-x py-medium-y ":
         size === "large",
-      "rounded-full text-20 font-medium px-medium-x py-normal-y ":
+      "rounded-full text-base lg:text-lg xl:text-20 font-medium px-8 xl:px-medium-x py-normal-y ":
         size === "medium",
       "rounded-full !text-base font-medium !px-8 !py-4 ": size === "small",
     },
--- src/sections/cta-section/index.jsx
+++ src/sections/cta-section/index.jsx
@@ -1,33 +1,43 @@
 import Button from "@/components/button";
 import { homePageMocks } from "@/mocks/home-page";
 import classnames from "classnames";
+import Image from "next/image";
 
 export default function CtaSection({ otherClasses }) {
-  const ctaSectionClasses = classnames(otherClasses, "mx-6");
+  const ctaSectionClasses = classnames(otherClasses, "mx-6 my-14 lg:my-20");
   const { cta } = homePageMocks;
   return (
     <section className={ctaSectionClasses} data-testid="cta-section">
-      <div className="max-w-1120 mx-auto px-6 bg-primary px-6 lg:px-28 py-16 rounded-2xl my-14 lg:my-20">
-        <h2 className="text-center text-4xl sm:text-5xl lg:text-65 font-bold order-1 sm:order-0 text-black font-instrument-sans uppercase text-center">
-          {cta.title}
-          <span className="font-medium italic">first?</span>
-        </h2>
-        <p className="text-xl font-normal my-6 font-instrument-sans text-center lg:px-32 ">
-          {cta.text}
-        </p>
-        <div className="flex md:flex-row flex-col items-center justify-center gap-5">
-          <Button
-            label="Book a Free Intro Call"
-            variant="secondary"
-            size="small"
-            otherClasses="w-full lg:w-auto"
-          />
-          <Button
-            label="Or send an email"
-            variant="outline"
-            size="small"
-            otherClasses="w-full lg:w-auto"
-          />
+      <div className="max-w-1120 mx-auto px-6 bg-primary px-6 lg:px-28 rounded-2xl relative">
+        <Image
+          src="/svg/cta-background.svg"
+          width={1120}
+          height={500}
+          alt="cta-background"
+          className="absolute top-0 left-0 w-full h-full object-cover"
+        />
+        <div className="relative py-16 ">
+          <h2 className="text-center text-4xl sm:text-5xl lg:text-65 font-bold order-1 sm:order-0 text-black font-instrument-sans uppercase text-center">
+            {cta.title}
+            <span className="font-medium italic">first?</span>
+          </h2>
+          <p className="text-xl font-normal my-6 font-instrument-sans text-center lg:px-32 ">
+            {cta.text}
+          </p>
+          <div className="flex md:flex-row flex-col items-center justify-center gap-5">
+            <Button
+              label="Book a Free Intro Call"
+              variant="secondary"
+              size="small"
+              otherClasses="w-full lg:w-auto"
+            />
+            <Button
+              label="Or send an email"
+              variant="outline"
+              size="small"
+              otherClasses="w-full lg:w-auto"
+            />
+          </div>
         </div>
       </div>
     </section>
