7e887a30c3505132e3114f5a40207db9d9384ecb
aqilusman45 <aqilusman45@gmail.com>
2025-03-14 12:32:53 +0500

    feat: complete ui and add content

--- src/sections/top-features/index.jsx
+++ src/sections/top-features/index.jsx
@@ -4,19 +4,19 @@ import Image from "next/image";
 
 export default function TopFeatures({ otherClasses }) {
   const topFeaturesClasses = classnames(otherClasses, "mx-6");
-  const { featureSections, plans } = homePageMocks;
+  const { featureSections, plansData } = homePageMocks;
 
   return (
     <section className={topFeaturesClasses} data-testid="top-features">
       <div className="max-w-1120 mx-auto overflow-hidden rounded-2xl my-14 lg:my-20 border border-black/10 ">
         <div className="overflow-x-auto">
-          <table className="min-w-[800px] w-full border-collapse">
+          <table className="min-w-800 w-full border-collapse">
             <thead>
-              <tr className="bg-[#FAFAFA] py-5">
+              <tr className="bg-gray-300 py-5">
                 <th className="p-4 text-left text-lg text-black font-instrument-sans font-semibold pl-10">
                   Top Features
                 </th>
-                {plans?.map((plan) => (
+                {plansData.plans.map((plan) => (
                   <th
                     key={plan.name}
                     className="p-4 text-center text-lg text-black font-instrument-sans font-semibold"
@@ -31,17 +31,17 @@ export default function TopFeatures({ otherClasses }) {
                 <tr
                   key={sectionIndex}
                   className={classnames(
-                    "border-t border-black/10  bg-[#FCFCFD] py-5",
-                    section?.header ? "bg-[#FCFCFD]" : "bg-white",
+                    "border-t border-black/10 py-5",
+                    section?.header ? "bg-gray-400" : "bg-white",
                   )}
                 >
                   <td className="p-4 font-normal font-instrument-sans text-base pl-10 text-black/60">
                     {section.title}
                   </td>
-                  {plans.map((plan, planIndex) => (
+                  {plansData.plans.map((plan, planIndex) => (
                     <td key={planIndex} className="p-4 text-center">
                       {section.isBoolean ? (
-                        plan[section.key || "features"][sectionIndex] ? (
+                        plan.features[section.data] ? (
                           <Image
                             src="/svg/pricing-tool-list.svg"
                             width={24}
@@ -59,8 +59,8 @@ export default function TopFeatures({ otherClasses }) {
                           />
                         )
                       ) : (
-                        <span className="px-4 block font-normal font-instrument-sans text-base text-black/60 block">
-                          {plan[section.data]}
+                        <span className="px-4 block font-normal font-instrument-sans text-base text-black/60">
+                          {plan.features[section.data]}
                         </span>
                       )}
                     </td>
