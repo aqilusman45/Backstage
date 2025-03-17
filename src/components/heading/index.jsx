e40ee05374953c94d9be6140851f7453dc1e1826
aqilusman45 <aqilusman45@gmail.com>
2025-03-17 15:34:11 +0500

    feat: add event-to-life section

--- src/components/heading/index.jsx
+++ src/components/heading/index.jsx
@@ -1,13 +1,15 @@
 import classnames from "classnames";
 
 export default function Heading({ otherClasses, title, subTitle }) {
-  const headingClasses = classnames(otherClasses);
-
   return (
-    <div className={headingClasses} data-testid="heading">
-      <h2 className="text-center text-4xl sm:text-5xl lg:text-65 font-bold order-1 sm:order-0 text-black font-instrument-sans uppercase text-center">
-        {title} <span className="font-medium italic">{subTitle}</span>
-      </h2>
-    </div>
+    <h2
+      data-testid="heading"
+      className={classnames(
+        otherClasses,
+        "text-center text-4xl sm:text-5xl lg:text-65 font-bold order-1 sm:order-0 text-black font-instrument-sans uppercase text-center",
+      )}
+    >
+      {title} <span className="font-medium italic">{subTitle}</span>
+    </h2>
   );
 }
